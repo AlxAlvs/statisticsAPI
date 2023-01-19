688445ae4c952bd899050e9a7068bdb3095e15ff
Alex Alves <alxalvs0121@gmail.com>
2023-01-19 11:36:39 -0300

    add a routine to clear expired transactions

---
+++ src/main/java/com/transaction/statistics/usecases/ClearExpiredTransactionsTimer.java
@@ -0,0 +1,43 @@
+package com.transaction.statistics.usecases;
+
+import com.transaction.statistics.entities.Transaction;
+import lombok.extern.slf4j.Slf4j;
+
+import java.time.Instant;
+import java.util.Timer;
+import java.util.TimerTask;
+
+import static com.transaction.statistics.repository.TransactionRepository.transactions;
+
+
+@Slf4j
+public class ClearExpiredTransactionsTimer extends TimerTask {
+
+    private static final int EXPIRE_SECONDS = 60;
+
+    static Timer timer = new Timer();
+
+    public final void run() {
+        removeExpiredTransactions();
+    }
+
+    public static void start(){
+        timer.schedule(new ClearExpiredTransactionsTimer(), 0, 1000);
+    }
+
+    public static void removeExpiredTransactions(){
+        transactions.values().removeIf(transactionItem -> checkIfOneMinuteOld(transactionItem));
+    }
+
+    private static boolean checkIfOneMinuteOld(Transaction mapItem) {
+        try {
+            Instant transactionTime = Instant.parse(mapItem.getTimestamp());
+            Instant now = Instant.now();
+            Instant maxTime = now.minusSeconds(EXPIRE_SECONDS);
+            return !(!transactionTime.isBefore(maxTime)) && transactionTime.isBefore(now);
+        } catch (Exception ex) {
+            log.error("Failed to check for expired transactions", ex);
+            return true;
+        }
+    }
+}
